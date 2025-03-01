0202885d2e036648f928358f8cf9c37bfb6259c1
CollarKey <mitrikovskyvlad@yandex.ru>
2025-03-01 18:08:51 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "Hello World")
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler)
+	http.ListenAndServe(":8080", router)
+}
